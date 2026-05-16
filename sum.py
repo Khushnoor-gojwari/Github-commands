403009236ae25ddce722183ac259b478baaca41c
Khushnoor-gojwari <kkhatim43@gmail.com>
2026-05-16 20:20:26 +0530

    added sum.py

---
+++ sum.py
@@ -0,0 +1,9 @@
+# sum of two numbers funtion
+def sum(a, b):
+    return a + b
+# sum of three numbers function
+def sum(a, b, c):
+    return a + b + c
+
+print(sum(1, 2)) # This will call the second sum function and return 6
+print(sum(1, 2, 3)) # This will call the second sum function
